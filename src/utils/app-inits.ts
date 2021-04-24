6b694e9843334708e8d4e9e9102c315a9e1fa221
Hamdi Bouallegue <hbouallegue@vermeg.com>
2021-04-24 11:36:20 +0100

    docs: update app-inits.ts

--- src/utils/app-inits.ts
+++ src/utils/app-inits.ts
@@ -11,14 +11,32 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
             realm: environment.keycloak.realm,
             clientId: environment.keycloak.clientId,
           },
+          // If set a false you canot get any information about the user exemple the username
+          // if you use keycloakService.getUserName()
+          // User not logged in or user profile was not loaded.
           loadUserProfileAtStartUp: true,
           initOptions: {
+            //   This is an action we specified on keycloak load
+            //   We have two options : 'login-required'|'check-sso'
+            //   If is set to 'login-required' this means your browser will do a full redirect to the Keycloak server and back to your application.
+            //   If is set to  'check-sso'  instead this action will be performed in a hidden iframe, so your application resources only need to be loaded and parsed once by the browser.
+            //   Then you will need to add the silentCheckSsoRedirectUri and create a html file   silent-check-sso.html with this content
+            // <html>
+            //    <body>
+            //         <script>
+            //           parent.postMessage(location.href, location.origin);
+            //         </script>
+            //      </body>
+            // </html>
             onLoad: 'login-required',
             checkLoginIframe: true,
+            // silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html',
           },
-          bearerExcludedUrls: [],
+          // By default the keycloak-angular library add Authorization: Bearer TOKEN to all http requests
+          // Then to exclude a list of URLs that should not have the authorization header we need to provide  them here.
+          bearerExcludedUrls: ['/assets'],
         });
-        resolve();
+        resolve(resolve);
       } catch (error) {
         reject(error);
       }
